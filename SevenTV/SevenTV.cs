0740771aa4d5db3d08aa7ed2c40782b53e73e11b
Xslash <kapimak28@gmail.com>
2022-10-28 15:41:10 +0200

    Add project files.

---
+++ SevenTV/SevenTV.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using SevenTV.Types;
+
+namespace SevenTV
+{
+    public class SevenTV
+    {
+        public readonly string _baseurl = "https://7tv.io/v3";
+        private HttpClient _client;
+
+        public SevenTV()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<EmoteSet?> GetEmoteSet(string ID)
+        {
+            string finalurl = _baseurl + $"/emote-sets/{ID}";
+            Uri uri = new Uri(finalurl);
+
+            var responseBody = await GetJSON(uri).ConfigureAwait(false);
+
+            if (responseBody == null)
+                return null;
+
+            EmoteSet? emoteset = new EmoteSet();
+            emoteset = JsonConvert.DeserializeObject<EmoteSet>(responseBody);
+
+            return emoteset;
+        }
+        public async Task<Emote?> GetEmote(string ID)
+        {
+            string finalurl = _baseurl + $"/emotes/{ID}";
+            Uri uri = new Uri(finalurl);
+
+            var responseBody = await GetJSON(uri).ConfigureAwait(false);
+
+            if (responseBody == null)
+                return null;
+
+            Emote? emote = new Emote();
+            emote = JsonConvert.DeserializeObject<Emote>(responseBody);
+
+            return emote;
+        }
+        public async Task<Connection?> GetConnection(ConnectionType type, string ID)
+        {
+            string finalurl = _baseurl + $"/users/{type}/{ID}";
+            Uri uri = new Uri(finalurl);
+
+            var responseBody = await GetJSON(uri).ConfigureAwait(false);
+
+            if (responseBody == null)
+                return null;
+
+            Connection? conn = new Connection();
+            conn = JsonConvert.DeserializeObject<Connection>(responseBody);
+
+            return conn;
+        }
+        public async Task<User?> GetUser(string ID)
+        {
+            string finalurl = _baseurl + $"/users/{ID}";
+            Uri uri = new Uri(finalurl);
+
+            var responseBody = await GetJSON(uri).ConfigureAwait(false);
+
+            if (responseBody == null)
+                return null;
+
+            User? user = new User();
+            user = JsonConvert.DeserializeObject<User>(responseBody);
+
+            return user;
+        }
+
+        private async Task<string?> GetJSON(Uri uri)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            var response = await _client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+                return null;
+
+            return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        }
+    }
+}
---
+++ SevenTV/Types/Types.cs
@@ -0,0 +1,85 @@
+﻿namespace SevenTV.Types
+{
+    public enum ConnectionType { TWITCH, YOUTUBE, DISCORD };
+    public class Connection
+    {
+        public string? id = null;
+        public string? platform = null;
+        public string? username = null;
+        public string? display_name = null;
+        public long linked_at = 0;
+        public int emote_capacity = 0;
+        public EmoteSet? emote_set = null;
+        public User? user = null;
+    }
+    public class User
+    {
+        public string? id = null;
+        public string? username = null;
+        public string? display_name = null;
+        public long createdAt = 0;
+        public string? avatar_url = null;
+        public string? biography = null;
+        public Style? style = null;
+        public Editor[]? editors = null;
+        public string[]? roles = null;
+        public Connection[]? connections = null;
+    }
+    public class Style
+    {
+        public int color = 0;
+        public string? paint = null;
+    }
+    public class Editor
+    {
+        public string? id = null;
+        public int permissions = 0;
+        public bool visible = false;
+        public long added_at = 0;
+    }
+    public class EmoteSet
+    {
+        public string? id = null;
+        public string? name = null;
+        public string[]? tags = null;
+        public bool immutable = false;
+        public bool privileged = false;
+        public Emote[]? emotes = null;
+        public int capacity = 0;
+        public User? owner = null;
+    }
+    public class Emote
+    {
+        public string? id = null;
+        public string? name = null;
+        public int flags = 0;
+        public long timestamp = 0;
+        public string? actor_id = null;
+        public EmoteData? data = null;
+    }
+    public class EmoteData
+    {
+        public string? id = null;
+        public string? name = null;
+        public int flags = 0;
+        public int lifecycle = 0;
+        public bool listed = false;
+        public bool animated = false;
+        public User? owner = null;
+        public EmoteHost? host = null;
+    }
+    public class EmoteHost
+    {
+        public string? url = null;
+        public EmoteFile[]? files = null;
+    }
+    public class EmoteFile
+    {
+        public string? name = null;
+        public string? static_name = null;
+        public int width = 0;
+        public int height = 0;
+        public long size = 0;
+        public string? format = null;
+    }
+}
